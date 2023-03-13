2d2cb4e4fa10f15bdc75adf35f58dad5def96d1e
Mari test <missuri82@gmail.com>
2023-03-13 21:18:29 +0300

    modified

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,8 +1,8 @@
 public class BonusMilesService {
     public int calculate(int price) {
         int result;
-        int b = 20;
-        result = price / b;
+        int money = 20;
+        result = price / money;
         return result;
     }
 }
